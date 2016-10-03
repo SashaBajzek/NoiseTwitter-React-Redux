9998e49c277c39c6ab728cfedc96b4e63e12052b
Sasha Bajzek <dabridginator@gmail.com>
2016-10-02 17:24:13 -0700

    Normalize preTweets before sending to reducer

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import makeStore from './store.js';
 import { Provider } from 'react-redux';
 import { AppContainer } from './containers/AppContainer';
 import { setTweets } from './action_creators';
-import { Schema, arrayOf } from 'normalizr';
+import { Schema, arrayOf, normalize } from 'normalizr';
 import './style.css';
 
 export const store = makeStore();
 
-const tweet = new Schema('tweets', { idAttribute: 'id' });
+const tweetSchema = new Schema('tweets', { idAttribute: 'id' });
+
+const preTweets = [{id: 0, avatar: "avatar.jpg", author: "JeffreyATW", time: "3h", tweetText: "I'm a little teapot, short and stout.", retweeted: false, liked: false},
+	{id: 1, avatar: "avatar.jpg", author: "JeffreyATW", time: "2h", tweetText: "Old McDonald had a farm.", retweeted: false, liked: false},
+	{id: 2, avatar: "avatar.jpg", author: "JeffreyATW", time: "1h", tweetText: "You give love a bad name.", retweeted: false, liked: false}];
+
+console.log(normalize(preTweets, tweetSchema));
 
 store.dispatch(setTweets(
-	[{id: 0, avatar: "avatar.jpg", author: "JeffreyATW", time: "3h", tweetText: "I'm a little teapot, short and stout.", retweeted: false, liked: false},
-	{id: 1, avatar: "avatar.jpg", author: "JeffreyATW", time: "2h", tweetText: "Old McDonald had a farm.", retweeted: false, liked: false},
-	{id: 2, avatar: "avatar.jpg", author: "JeffreyATW", time: "1h", tweetText: "You give love a bad name.", retweeted: false, liked: false}]
+	normalize(preTweets, tweetSchema)
 ));
 
 ReactDOM.render(
